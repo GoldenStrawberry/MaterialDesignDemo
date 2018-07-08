ba66b1a445439930efe4948990159ef5dcab792e
GoldenStrawberry <793207695@qq.com>
2018-07-08 17:11:18 +0800

    使用getRawX()/getRawY()解决拖动View抖动

--- coordinatorlayout_demo/src/main/java/github/goldenstrawberry/materialdesigndemo/DragTextView.java
+++ coordinatorlayout_demo/src/main/java/github/goldenstrawberry/materialdesigndemo/DragTextView.java
@@ -7,12 +7,19 @@ import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.ViewConfiguration;
+import android.widget.Scroller;
 import android.widget.TextView;
 
 /**
  * Created by Ryan.Hoo
  * Date : 2018/7/8.10:48
  * Motto : Working hard to write logically clear code
+ *
+ * 拖移View时的抖动问题:
+ * https://blog.csdn.net/u010335298/article/details/51891653
+ * 我的理解 是由于getX()和getY()在第一次是getLeft和getTop，
+ * 之后加上了TranslationX和TranslationY导致的
+ * 使用getRawX()、getRawY()是pointer相对于整个screen的绝对坐标。
  */
 
 public class DragTextView extends AppCompatTextView {
@@ -31,34 +38,34 @@ public class DragTextView extends AppCompatTextView {
 
     public DragTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        init(context);
     }
 
-    private void init() {
+    private void init(Context context) {
         // 最小滑动距离
-        touchSlop = ViewConfiguration.get(getContext()).getScaledTouchSlop();
+        touchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()){
             case MotionEvent.ACTION_DOWN:
-                downX = event.getX();
-                downY = event.getY();
+                downX = event.getRawX();
+                downY = event.getRawY();
                 break;
             case MotionEvent.ACTION_MOVE:
-                int distanceX = (int) (event.getX() - downX);
-                int distanceY = (int) (event.getY() - downY);
+                int distanceX = (int) (event.getRawX() - downX);
+                int distanceY = (int) (event.getRawY() - downY);
                 if(Math.abs(distanceX) > touchSlop || Math.abs(distanceY) > touchSlop){
                     ViewCompat.offsetTopAndBottom(this,distanceY);
                     ViewCompat.offsetLeftAndRight(this,distanceX);
-                    downX = event.getX() ;
-                    downY = event.getY() ;
+                    downX = event.getRawX() ;
+                    downY = event.getRawY() ;
                 }
                 break;
             case MotionEvent.ACTION_UP:
-                downX = event.getX();
-                downY = event.getY();
+                downX = event.getRawX();
+                downY = event.getRawY();
                 break;
         }
         return true;
