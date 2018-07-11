efbbe9a92a6e5018e5d1197e373737277b6fb64b
huwei <793207695@qq.com>
2018-07-11 19:38:27 +0800

    添加appbarlayout

--- appbarlayout_demo/src/main/java/github/goldenstrawberry/appbarlayout_demo/MainActivity.java
+++ appbarlayout_demo/src/main/java/github/goldenstrawberry/appbarlayout_demo/MainActivity.java
@@ -1,7 +1,9 @@
 package github.goldenstrawberry.appbarlayout_demo;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //设置标题文字
+        toolbar.setTitle("巅峰榜 流行指数 第64天");
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+        // 显示返回箭头
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
